fe6d3f4686ec793bdc7b9f75ac42daf75986cbc7
Aaron <aaron.johnson_tx@yahoo.com>
2016-12-03 16:29:52 -0600

    formatting

--- public/javascripts/timesheetApp.js
+++ public/javascripts/timesheetApp.js
@@ -70,54 +70,49 @@ function updateIndices(data, scope){
   //col starts at 0(A) & rowIndex starts at 1 so 1 = first row
   var dateCol, updated;
   if(data.length > 0){
-    for(rowIndex = 0; rowIndex < data.length; rowIndex++){
+    for(var rowIndex = 0; rowIndex < data.length; rowIndex++){
       var row = data[rowIndex];
-        for(colIndex = 0; colIndex <= row.length; colIndex++){
-          var col = row[colIndex];
-          if(col === 'Date'){
-            scope.indices.firstEntryCell = {row: rowIndex, col: colIndex};
-            dateCol = colIndex;
-          }
+      for(var colIndex = 0; colIndex <= row.length; colIndex++){
+        var col = row[colIndex];
+        if(col === 'Date'){
+          scope.indices.firstEntryCell = {row: rowIndex, col: colIndex};
+          dateCol = colIndex;
+        }
 
-          if(col === 'Hours'){
-            scope.indices.hoursCol = colIndex;
-          }
+        if(col === 'Hours'){
+          scope.indices.hoursCol = colIndex;
+        }
 
-          if(col === 'Description'){
-            scope.indices.descCol = colIndex;
-          }
-        };
+        if(col === 'Description'){
+          scope.indices.descCol = colIndex;
+        }
+      }
 
-        if(scope.indices.firstEntryCell && rowIndex > scope.indices.firstEntryCell.row){
+      if(scope.indices.firstEntryCell && rowIndex > scope.indices.firstEntryCell.row){
           //if we are in the entry section and we find an empty row, it is the last entry
-          if((!row || row.length === 0) && !updated){
+        if((!row || row.length === 0) && !updated){
 
             //If the old last index is longer than the current one (user deleted some entries), then
             //we want to remove the extra elements.
-            if(scope.indices.lastEntryCell && scope.indices.lastEntryCell.row > rowIndex){
-              scope.entries.slice(0,rowIndex);
-            }
-            //now we'll update to the new index
-            scope.indices.lastEntryCell = {row: rowIndex , col: dateCol};
-            updated = true;
+          if(scope.indices.lastEntryCell && scope.indices.lastEntryCell.row > rowIndex){
+            scope.entries.slice(0,rowIndex);
           }
-          if(!updated){
-            var obj = {
-              date: data[rowIndex][dateCol],
-              desc: data[rowIndex][scope.indices.descCol],
-              hours: data[rowIndex][scope.indices.hoursCol]
-            };
+          //now we'll update to the new index
+          scope.indices.lastEntryCell = {row: rowIndex , col: dateCol};
+          updated = true;
+        }
+        if(!updated){
+          var obj = {
+            date: data[rowIndex][dateCol],
+            desc: data[rowIndex][scope.indices.descCol],
+            hours: data[rowIndex][scope.indices.hoursCol]
+          };
 
-            scope.entries[rowIndex - scope.indices.firstEntryCell.row - 1] = obj;
-            console.log('added obj at ' + rowIndex + ' date is ' + obj.date);
-          }
+          scope.entries[rowIndex - scope.indices.firstEntryCell.row - 1] = obj;
+          console.log('added obj at ' + rowIndex + ' date is ' + obj.date);
+        }
       }
-    };
+    }
   }
   console.log('updated');
-}
-
-function idOf(i) {
-    return (i >= 26 ? idOf((i / 26 >> 0) - 1) : '') +
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[i % 26 >> 0];
 }
